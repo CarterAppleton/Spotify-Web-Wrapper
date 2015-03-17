33dee10b82f6e0b245d1d19de6f118d4f4dad473
Carter Appleton <carter.appleton@gmail.com>
2015-03-17 13:50:08 -0400

    Initial Commit

---
+++ SpotifyWebWrapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotifyWebWrapper
+//
+//  Created by Carter Appleton on 3/17/15.
+//  Copyright (c) 2015 Carter Appleton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
